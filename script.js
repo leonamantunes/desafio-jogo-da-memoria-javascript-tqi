54610cbe91d0827e74cb5debb6d7d4d41fa529bf
Leonam Antunes <leonamc.antunes@gmail.com>
2022-06-05 18:08:59 -0300

    Notas

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
-//função para virar carta
+//função 01 -- para virar carta
 function flipCard() {
     if(lockBoard) return;
     if(this === firstCard) return;
@@ -20,7 +20,7 @@ function flipCard() {
     checkForMatch();
 }
 
-//função que checa se as cartas são iguais
+//função 02 -- checa se as cartas são iguais
 function checkForMatch() {
     if(firstCard.dataset.card === secondCard.dataset.card) {
         disableCards();
@@ -30,7 +30,7 @@ function checkForMatch() {
     unflipCards();
 }
 
-//função que desabilita as cartas
+//função 03 -- desabilita as cartas
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
@@ -38,7 +38,7 @@ function disableCards() {
     resetBoard();
 }
 
-//funcão que desvira as cartas
+//funcão 04 -- desvira as cartas
 function unflipCards() {
     lockBoard = true;
 
@@ -50,13 +50,13 @@ function unflipCards() {
     }, 1500);
 }
 
-//função que reseta o tabuleiro
+//função 05 -- reseta o tabuleiro
 function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
 
-//função para embaralhar as cartas
+//função 06 -- embaralha as cartas
 (function shuffle() {
     cards.forEach((card) => {
         let ramdomPosition = Math.floor(Math.random() * 12);
